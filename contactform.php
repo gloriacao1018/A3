479d5b50d8208524110a1a3498c05bbd50150f4f
gloriacao1018 <caoxiao@sheridancollege.ca>
2023-11-18 16:37:05 -0500

    contact form

---
+++ contactform.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<form action="process-contact.php" method="POST">    
+    First Name: <input type="text" name="fName">
+    Last Name: <input type="text" name="lName">
+    Email: <input type="text" name="email">
+    Phone Number: <input type="text" name="phone_num">
+    Message: <textarea name="message"></textarea>
+    <input type="submit" value="Submit">
+</form>
---
+++ process-contact.php
@@ -0,0 +1,36 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$fName = $_POST['fName'];
+$lName = $_POST['lName'];
+$email = $_POST['email'];
+$phone_num = $_POST['phone_num'];
+$message = $_POST['message'];
+
+$dsn = 'mysql:host=localhost;dbname=IMMNEWSNETWORK;charset=utf8mb4';
+$dbusername = 'root';
+$dbpassword = '';
+
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+
+$stmt = $pdo->prepare("INSERT INTO `contactform` (`fName`, `lName`, `email`, `phone_num`, `message`, `created_at`) VALUES (?, ?, ?, ?, ?, current_timestamp())");
+
+$stmt->execute([$fName, $lName, $email, $phone_num, $message]);
+
+
+if($stmt->execute()){ 
+?>
+<h1> Thank you for your message!</h1>
+<?php
+
+}else{  
+?><h1>Error</h1> <?php
+}
+?>
+
+
